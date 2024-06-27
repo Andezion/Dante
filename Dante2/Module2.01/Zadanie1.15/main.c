64386d3201fccdd7d8c32e3780d6f3d2ebdff40b
Andezion <115638748+Andezion@users.noreply.github.com>
2024-06-27 19:31:50 +0300

    Update main.c

--- Dante2/Module2.01/Zadanie1.15/main.c
+++ Dante2/Module2.01/Zadanie1.15/main.c
@@ -1,1 +1,56 @@
+#include <stdio.h>
 
+int main()
+{
+    char name[31];
+
+    printf("Enter name: ");
+    scanf("%30[^\n]",name);
+
+    FILE *file;
+    file = fopen(name, "rb");
+    if(!file)
+    {
+        printf("Couldn't open file");
+        return 4;
+    }
+
+    while(getchar() != '\n');
+    printf("\n");
+
+    int temp;
+
+    fseek(file, 0, SEEK_SET);
+
+    int pointer = 0;
+    while((temp = getc(file)) != EOF)
+    {
+        pointer = 1;
+    }
+    if(pointer == 0)
+    {
+        printf("%i", 0);
+        fclose(file);
+        return 0;
+    }
+
+    fseek(file, 0, SEEK_SET);
+
+    int counter = 1;
+    printf("%i. ", counter);
+
+    while ((temp = getc(file)) != EOF)
+    {
+        putchar(temp);
+        if (temp == '\n')
+        {
+            counter++;
+            printf("%i. ", counter);
+        }
+    }
+
+    printf("\n%i",counter);
+
+    fclose(file);
+    return 0;
+}
