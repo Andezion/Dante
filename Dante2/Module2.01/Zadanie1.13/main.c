c6f9f9dbc2e3b92a6c184f4d761eb2df84dda5e5
Andezion <115638748+Andezion@users.noreply.github.com>
2024-06-27 19:30:04 +0300

    Update main.c

--- Dante2/Module2.01/Zadanie1.13/main.c
+++ Dante2/Module2.01/Zadanie1.13/main.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+int main()
+{
+    FILE *file;
+    char name[31];
+
+    printf("Enter: ");
+    scanf("%30[^\n]",name);
+
+    file = fopen(name, "rb");
+    if(!file)
+    {
+        printf("Couldn't open file");
+        return 4;
+    }
+
+    int temp;
+    while((temp = fgetc(file)) != EOF)
+    {
+        printf("%i ", temp);
+    }
+
+    fclose(file);
+    return 0;
+}
