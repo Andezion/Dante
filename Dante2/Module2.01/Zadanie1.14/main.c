2bc4c4e68d804703ef93606b9655dba5a96e2a59
Andezion <115638748+Andezion@users.noreply.github.com>
2024-06-27 19:30:55 +0300

    Update main.c

--- Dante2/Module2.01/Zadanie1.14/main.c
+++ Dante2/Module2.01/Zadanie1.14/main.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+#include <unistd.h>
 
+int main()
+{
+    char name[31];
+
+    printf("Enter name: ");
+    scanf("%30[^\n]",name);
+
+    FILE *file;
+    file = fopen(name, "a+");
+    if(!file)
+    {
+        printf("Couldn't create file");
+        return 5;
+    }
+
+    while(getchar() != '\n');
+    fseek(file, -1, SEEK_SET);
+
+    int temp;
+    printf("Enter text:\n");
+    
+    while ((temp = getchar()) != '\0' && temp != EOF)
+    {
+        fputc(temp, file);
+    }
+
+    fseek(file, -1, SEEK_END);
+    ftruncate(fileno(file), ftell(file));
+
+    fclose(file);
+
+    printf("File saved");
+
+    return 0;
+}
