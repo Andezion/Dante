48b972f24588c6fd656f2ba9e80818975cb6b935
Andezion <115638748+Andezion@users.noreply.github.com>
2024-06-27 19:28:55 +0300

    Update main.c

--- Dante2/Module2.01/Zadanie1.11/main.c
+++ Dante2/Module2.01/Zadanie1.11/main.c
@@ -1,1 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    FILE *file;
+    char name[31];
+    srand(time(NULL));
+
+    printf("Enter: ");
+    scanf("%30[^\n]",name);
+
+    file = fopen(name,"wb");
+    if(!file)
+    {
+        printf("Couldn't create file");
+        return 5;
+    }
+
+    while(getchar() != '\n');
+
+    for (int i = 0; i < 100; i++)
+    {
+        int num1 = -100 + rand() % 201;
+        int num2 = -10 + rand() % 20;
+
+        float temp;
+        if (num2 != 0)
+        {
+            temp = (float) num1 / (float) num2;
+        }
+        else
+        {
+            temp = 0;
+        }
+        fwrite(&temp, sizeof(float), 1, file);
+    }
+
+    printf("File saved");
+    fclose(file);
+    return 0;
+}
 
