f4e5a636d0c5ab4c8c9b282f6f5ab192a47a5211
Andezion <115638748+Andezion@users.noreply.github.com>
2024-06-27 19:22:16 +0300

    Update main.c

--- Dante2/Module2.01/Zadanie1.04/main.c
+++ Dante2/Module2.01/Zadanie1.04/main.c
@@ -1,1 +1,47 @@
+#include <stdio.h>
+#include <string.h>
 
+int main()
+{
+    FILE *first_file;
+    char filename1[31];
+    printf("Enter: ");
+
+    scanf("%30[^\n]",filename1);
+    first_file = fopen(filename1, "rb");
+    if(!first_file)
+    {
+        printf("Couldn't open file");
+        return 4;
+    }
+
+    while(getchar() != '\n');
+
+    FILE *second_file;
+    char filename2[31];
+    printf("Enter: ");
+
+    scanf("%30[^\n]",filename2);
+    second_file = fopen(filename2, "wb");
+    if(!second_file)
+    {
+        printf("Couldn't create file");
+        fclose(first_file);
+        return 5;
+    }
+
+    fseek(first_file, 0, SEEK_END);
+    int size = ftell(first_file);
+    for(int i = size - 1; i >= 0; i--)
+    {
+        fseek(first_file, i, SEEK_SET);
+        int temp = fgetc(first_file);
+        fputc(temp, second_file);
+    }
+
+    printf("File copied");
+    fclose(first_file);
+    fclose(second_file);
+
+    return 0;
+}
