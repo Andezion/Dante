e68e5a1207a3891d80ae968d1c44a574880b1b0c
Andezion <115638748+Andezion@users.noreply.github.com>
2024-06-27 19:32:24 +0300

    Update main.c

--- Dante2/Module2.01/Zadanie1.16/main.c
+++ Dante2/Module2.01/Zadanie1.16/main.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
 
+int main()
+{
+    FILE *file;
+    char name[31];
+
+    printf("Enter: ");
+    scanf("%30[^\n]",name);
+
+    file = fopen(name, "r");
+    if (file != NULL)
+    {
+        printf("File already exists!");
+        fclose(file);
+        return 0;
+    }
+
+    file = fopen(name, "w");
+    if (!file)
+    {
+        printf("Couldn't create file");
+        return 5;
+    }
+
+    printf("File created");
+
+    fclose(file);
+    return 0;
+}
