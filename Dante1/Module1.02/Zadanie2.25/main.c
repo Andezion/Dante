768b9b431975160417bb31d70c7245bccab51c7d
Andezion <115638748+Andezion@users.noreply.github.com>
2024-09-02 00:56:30 +0300

    Update main.c

--- Dante1/Module1.02/Zadanie2.25/main.c
+++ Dante1/Module1.02/Zadanie2.25/main.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include <ctype.h>
 
+int main()
+{
+    int number;
+    printf("Enter smth: ");
+
+    int test = scanf("%d", &number);
+
+    int result = (number > 0) * 1 + (number < 0) * 2 + (number == 0) * 3 + (test == 1) * 1;
+
+    switch(result)
+    {
+        case 2:
+            printf("%i is positive", number);
+            return 0;
+        case 3:
+            printf("%i is negative", number);
+            return 0;
+        case 4:
+            printf("%i is equal to 0", number);
+            return 0;
+        default:
+            printf("Incorrect input");
+            return 1;
+    }
+}
