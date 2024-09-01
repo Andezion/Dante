8bdb7c0d3dad37e49b34fe1e3b1ad38681d83c6c
Andezion <115638748+Andezion@users.noreply.github.com>
2024-09-02 00:28:43 +0300

    Update main.c

--- Dante1/Module1.01/Zadanie1.19/main.c
+++ Dante1/Module1.01/Zadanie1.19/main.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    float num;
+
+    printf("Enter number:");
+
+    int check = scanf("%f",&num);
+    if(check != 1)
+    {
+        return 1;
+    }
+
+    printf("Result is = %f",2 * num * num * num - 4 * num * num + 3 * num - 7);
+    return 0;
+}
