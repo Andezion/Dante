eb6cccd01495e7cda94b53467be5fd11e0466e99
Andezion <115638748+Andezion@users.noreply.github.com>
2024-09-02 00:34:21 +0300

    Update main.c

--- Dante1/Module1.01/Zadanie1.29/main.c
+++ Dante1/Module1.01/Zadanie1.29/main.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+
+int main()
+{
+    int num1, num2;
+    printf("Enter numbers: ");
+    scanf("%i %i", &num1, &num2);
+
+    int max = (num1 + num2 + abs(num1 - num2)) / 2;
+    int min = (num1 + num2 - abs(num1 - num2)) / 2;
+    printf("Max: %i\n",max);
+    printf("Min: %i\n",min);
+
+    return 0;
+}
