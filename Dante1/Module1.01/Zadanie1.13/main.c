2ea7673551435a760709461699fcd8d79dfdb4f8
Andezion <115638748+Andezion@users.noreply.github.com>
2024-09-02 00:22:55 +0300

    Update main.c

--- Dante1/Module1.01/Zadanie1.13/main.c
+++ Dante1/Module1.01/Zadanie1.13/main.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+int main()
+{
+    int num1, num2;
+
+    printf("Enter 1-st number:");
+    int check1 = scanf("%d",&num1);
+    if(check1 != 1)
+    {
+        return 1;
+    }
+
+    printf("Enter second number:");
+    int check2 = scanf("%d",&num2);
+    if(check2 != 1)
+    {
+        return 1;
+    }
+    
+    printf("%i\n", num1 + num2);
+    printf("%i\n", num1 - num2);
+    printf("%i\n", num1 * num2);
+    printf("%i", num1 / num2);
+    return 0;
+}
