664dc4fae29d178a62c0286f52cf791cbadc6603
Andezion <115638748+Andezion@users.noreply.github.com>
2024-09-02 00:30:44 +0300

    Update main.c

--- Dante1/Module1.01/Zadanie1.22/main.c
+++ Dante1/Module1.01/Zadanie1.22/main.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main()
+{
+    float number;
+
+    printf("Enter number: ");
+    scanf("%f",&number);
+
+    int num;
+    printf("Enter num: ");
+    scanf("%i",&num);
+
+    printf("%.*f",num,number);
+
+    return 0;
+}
