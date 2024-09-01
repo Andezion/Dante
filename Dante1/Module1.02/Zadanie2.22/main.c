539e5a66031567e9a250780b9b50bb41e7a15f9e
Andezion <115638748+Andezion@users.noreply.github.com>
2024-09-02 00:55:11 +0300

    Update main.c

--- Dante1/Module1.02/Zadanie2.22/main.c
+++ Dante1/Module1.02/Zadanie2.22/main.c
@@ -1,1 +1,60 @@
+#include <stdio.h>
 
+int main()
+{
+    int num;
+
+    printf("Enter: ");
+
+    int check = scanf("%i",&num);
+
+    printf("\n");
+
+    int try = (check == 1);
+    switch(try)
+    {
+        case 1:
+            switch (num)
+            {
+                case 0:
+                    printf("zero");
+                    return 0;
+                case 1:
+                    printf("jeden");
+                    return 0;
+                case 2:
+                    printf("dwa");
+                    return 0;
+                case 3:
+                    printf("trzy");
+                    return 0;
+                case 4:
+                    printf("cztery");
+                    return 0;
+                case 5:
+                    printf("piec");
+                    return 0;
+                case 6:
+                    printf("szesc");
+                    return 0;
+                case 7:
+                    printf("siedem");
+                    return 0;
+                case 8:
+                    printf("osiem");
+                    return 0;
+                case 9:
+                    printf("dziewiec");
+                    return 0;
+                case 10:
+                    printf("dziesiec");
+                    return 0;
+                default:
+                    printf("Incorrect input");
+                    return 1;
+            }
+        default:
+            printf("Incorrect input");
+            return 1;
+    }
+}
