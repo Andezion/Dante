2bcf40fa43f673ad8b0cc36ce5a5f624294c1788
Andezion <115638748+Andezion@users.noreply.github.com>
2024-09-02 00:22:17 +0300

    Update main.c

--- Dante1/Module1.01/Zadanie1.12/main.c
+++ Dante1/Module1.01/Zadanie1.12/main.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main()
+{
+    float number;
+    printf("Enter number: ");
+
+    int check = scanf("%f",&number);
+    if(check != 1)
+    {
+        printf("Incorrect input");
+        return 1;
+    }
+
+    printf("%f\n%f",number, number * 10);
+    return 0;
+}
