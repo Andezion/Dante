ccdd82af0b909799b139651e503be1b2fef4f925
Andezion <115638748+Andezion@users.noreply.github.com>
2024-09-02 00:41:11 +0300

    Update main.c

--- Dante1/Module1.02/Zadanie2.01/main.c
+++ Dante1/Module1.02/Zadanie2.01/main.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
+int main()
+{
+    int num1, num2;
+    printf("Enter numbers: ");
+
+    int check = scanf("%i %i",&num1,&num2);
+    if(check != 2)
+    {
+        printf("Incorrect input");
+        return 1;
+    }
+    printf("%i",num1 + num2);
+    return 0;
+}
