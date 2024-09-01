29f8f5be9990caeeba618b2ab2d6ad5aca1c0896
Andezion <115638748+Andezion@users.noreply.github.com>
2024-09-02 00:50:02 +0300

    Update main.c

--- Dante1/Module1.02/Zadanie2.15/main.c
+++ Dante1/Module1.02/Zadanie2.15/main.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
 
+int main()
+{
+    float num1, num2;
+    printf("Enter: ");
+
+    int check1 = scanf("%f",&num1);
+    if(check1 != 1)
+    {
+        printf("Incorrect input");
+        return 1;
+    }
+
+    printf("\nEnter: ");
+    int check2 = scanf("%f",&num2);
+    if(check2 != 1)
+    {
+        printf("Incorrect input");
+        return 1;
+    }
+
+    if((num1 + num2) * (num1 - num2) == num1 * num1 + num2 * num2)
+    {
+        printf("\ntozsamosc jest prawdziwa");
+    }
+    else
+    {
+        printf("\ntozsamosc nie jest prawdziwa");
+    }
+
+    return 0;
+}
