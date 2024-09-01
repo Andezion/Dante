8be336e8e9cc73c2cb6d96db1c653f04302afd17
Andezion <115638748+Andezion@users.noreply.github.com>
2024-09-02 00:33:25 +0300

    Update main.c

--- Dante1/Module1.01/Zadanie1.27/main.c
+++ Dante1/Module1.01/Zadanie1.27/main.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main()
+{
+    int num1, num2;
+    char type;
+
+    printf("Enter number: ");
+
+    int num3 = scanf("%i%ci%i",&num1,&type,&num2);
+    printf("%i%c%ii\n%i",num1,type,num2,num3);
+    return 0;
+}
