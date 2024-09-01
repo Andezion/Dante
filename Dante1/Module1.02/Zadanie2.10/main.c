be39b2b623d19d8e38ae54d12945f78cac0c0616
Andezion <115638748+Andezion@users.noreply.github.com>
2024-09-02 00:46:54 +0300

    Update main.c

--- Dante1/Module1.02/Zadanie2.10/main.c
+++ Dante1/Module1.02/Zadanie2.10/main.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
 
+int main()
+{
+    float num1, num2;
+
+    printf("Enter 1-st number:");
+    int check1 = scanf("%f",&num1);
+    if(check1 != 1)
+    {
+        printf("Incorrect input");
+        return 1;
+    }
+
+    printf("Enter second number:");
+    int check2 = scanf("%f",&num2);
+    if(check2 != 1)
+    {
+        printf("Incorrect input");
+        return 1;
+    }
+
+    printf("%f\n", num1 + num2);
+    printf("%f\n", num1 - num2);
+    printf("%f\n", num1 * num2);
+    if(num2 == 0)
+    {
+        printf("Operation not permitted");
+    }
+    else
+    {
+        printf("%f", num1 / num2);
+    }
+    return 0;
+}
