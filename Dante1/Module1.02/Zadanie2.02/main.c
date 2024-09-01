afb63d6e6cd2e16524ba9fa53a1eb20eea0276d5
Andezion <115638748+Andezion@users.noreply.github.com>
2024-09-02 00:42:04 +0300

    Update main.c

--- Dante1/Module1.02/Zadanie2.02/main.c
+++ Dante1/Module1.02/Zadanie2.02/main.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
+int main()
+{
+    int num1, num2, num3, num4;
+
+    printf("Podaj numer telefonu: ");
+    int scan = scanf("(%d) %d-%d-%d", &num1, &num2, &num3, &num4);
+    if (scan < 4)
+    {
+        printf("Incorrect input");
+        return 1;
+    }
+    printf("Numer kierunkowy: %.2d\n", num1);
+    printf("Numer telfonu: %.3d-%.2d-%.2d", num2, num3, num4);
+    return 0;
+}
