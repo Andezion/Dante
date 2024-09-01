065ac7963feb7d728ebdf6029e8187828b436114
Andezion <115638748+Andezion@users.noreply.github.com>
2024-09-02 00:27:25 +0300

    Update main.c

--- Dante1/Module1.01/Zadanie1.17/main.c
+++ Dante1/Module1.01/Zadanie1.17/main.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <math.h>
 
+int main()
+{
+    float x1,x2,x3,x4,x5,x6,x7,x8,x9,x10;
+
+    printf("Enter numbers:");
+    int check = scanf("%f %f %f %f %f %f %f %f %f %f",&x1,&x2,&x3,&x4,&x5,&x6,&x7,&x8,&x9,&x10);
+    if(check != 10)
+    {
+        return 1;
+    }
+
+    float y = (x1+x2+x3+x4+x5+x6+x7+x8+x9+x10)/10;
+    printf("Result is = %.4f",y);
+    return 0;
+}
