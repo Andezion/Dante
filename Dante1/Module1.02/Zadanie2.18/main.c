2ca6ae6d9a6597761796175ffdd79cd8faade9ab
Andezion <115638748+Andezion@users.noreply.github.com>
2024-09-02 00:53:04 +0300

    Update main.c

--- Dante1/Module1.02/Zadanie2.18/main.c
+++ Dante1/Module1.02/Zadanie2.18/main.c
@@ -1,1 +1,40 @@
+#include <stdio.h>
 
+int main()
+{
+    int number;
+
+    printf("Enter: ");
+    int check = scanf("%i",&number);
+    if(check != 1)
+    {
+        printf("Incorrect input");
+        return 1;
+    }
+
+    printf("\n");
+
+    if(number >= 0 && number <= 10)
+    {
+        printf("ocena 2");
+    }
+    else if(number >= 11 && number <= 13)
+    {
+        printf("ocena 3");
+    }
+    else if(number >= 14 && number <= 16)
+    {
+        printf("ocena 4");
+    }
+    else if(number >= 17 && number <= 20)
+    {
+        printf("ocena 5");
+    }
+    else
+    {
+        printf("Incorrect input");
+        return 1;
+    }
+
+    return 0;
+}
