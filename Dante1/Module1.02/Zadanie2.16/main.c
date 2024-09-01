53e5020d23edf0567466384d8d62e2040432ce8d
Andezion <115638748+Andezion@users.noreply.github.com>
2024-09-02 00:50:56 +0300

    Update main.c

--- Dante1/Module1.02/Zadanie2.16/main.c
+++ Dante1/Module1.02/Zadanie2.16/main.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+int main()
+{
+    float number;
+
+    printf("Enter: ");
+    int check = scanf("%f",&number);
+    if(check != 1)
+    {
+        printf("Incorrect input");
+        return 1;
+    }
+
+    if(number > 0)
+    {
+        printf("\ndodatnia");
+    }
+    else if(number == 0)
+    {
+        printf("\nzerowa");
+    }
+    else
+    {
+        printf("\nujemna");
+    }
+
+    return 0;
+}
