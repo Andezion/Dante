e0cadabf307961c74896bae7e0d01e150352b295
Andezion <115638748+Andezion@users.noreply.github.com>
2024-09-02 00:53:21 +0300

    Update main.c

--- Dante1/Module1.02/Zadanie2.19/main.c
+++ Dante1/Module1.02/Zadanie2.19/main.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
 
+int main()
+{
+    char l1,l2,l3,l4,l5;
+
+    printf("Enter: ");
+
+    scanf("%c%c%c%c%c",&l1,&l2,&l3,&l4,&l5);
+
+    printf("\n");
+
+    l1 = toupper(l1);
+    l2 = toupper(l2);
+    l3 = toupper(l3);
+    l4 = toupper(l4);
+    l5 = toupper(l5);
+
+    printf("%c%c%c%c%c",l1,l2,l3,l4,l5);
+    
+    return 0;
+}
