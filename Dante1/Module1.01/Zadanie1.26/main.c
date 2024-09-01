a8f67840a34c79f841c23abd520f381a5ca5a392
Andezion <115638748+Andezion@users.noreply.github.com>
2024-09-02 00:32:49 +0300

    Update main.c

--- Dante1/Module1.01/Zadanie1.26/main.c
+++ Dante1/Module1.01/Zadanie1.26/main.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int x, y, z;
 
+    printf("Enter data: ");
+    scanf("%d-%d-%d",&x,&y,&z);
+    printf("Day:%6s%02d\nMonth:%4s%02d\nYear:%3s%04d", "",x, "",y, "",z);
+    return 0;
+}
