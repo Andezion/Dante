9b436e75b195ac070b468da0198114b75cad345f
Andezion <115638748+Andezion@users.noreply.github.com>
2024-09-02 00:28:00 +0300

    Update main.c

--- Dante1/Module1.01/Zadanie1.18/main.c
+++ Dante1/Module1.01/Zadanie1.18/main.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    float num1, num2, num3, num4, num5;
+    printf("Enter 5 numbers:");
+
+    int check = scanf("%f %f %f %f %f", &num1, &num2, &num3, &num4, &num5);
+    if(check != 5)
+    {
+        return 1;
+    }
+
+    printf("Wynik: %.4f",((num1 + num2 + num3 + num4 + num5) / 5));
+    return 0;
+}
