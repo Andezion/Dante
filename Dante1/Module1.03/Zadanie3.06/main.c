3113b7e868685cfadad85072d98eddf365aa7e2c
Andezion <115638748+Andezion@users.noreply.github.com>
2024-09-11 15:16:39 +0300

    Update main.c

--- Dante1/Module1.03/Zadanie3.06/main.c
+++ Dante1/Module1.03/Zadanie3.06/main.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+int main()
+{
+    unsigned long long int result = 1;
+    int i;
+
+    for (i = 0; i <= 19; i++)
+        printf("%llu ", result), result *= 10;
+
+    return 0;
+}
