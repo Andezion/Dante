01f254b1669a98f390ea65d100c0e54d563510e2
Andezion <115638748+Andezion@users.noreply.github.com>
2024-09-18 22:41:44 +0300

    Update main.c

--- Dante1/Module1.05/Zadanie5.18/main.c
+++ Dante1/Module1.05/Zadanie5.18/main.c
@@ -1,1 +1,40 @@
+#include <stdio.h>
+#include <string.h>
 
+int main()
+{
+    char input_text[51];
+    int token = 0;
+
+    printf("Enter: ");
+    scanf("%50s", input_text);
+
+    for (size_t i = 0; i < strlen(input_text); i++)
+    {
+        if (input_text[i] == '(')
+        {
+            token++;
+        }
+        else if (input_text[i] == ')')
+        {
+            token--;
+        }
+
+        if (token < 0)
+        {
+            printf("\nNiepoprawny");
+            return 0;
+        }
+    }
+
+    if (token == 0)
+    {
+        printf("\nPoprawny");
+    }
+    else
+    {
+        printf("\nNiepoprawny");
+    }
+
+    return 0;
+}
