f2e76c53977db81893df854839f84e5750659290
Andezion <115638748+Andezion@users.noreply.github.com>
2025-04-17 13:53:45 +0200

    Create functions.h

---
+++ Dante2/Module2.07/Zadanie1.12/functions.h
@@ -0,0 +1,10 @@
+#ifndef SCRABBLE_FUNCTIONS_H
+#define SCRABBLE_FUNCTIONS_H
+
+void print_chicken(void);
+void print_egg(void);
+void print_rabbit(void);
+
+void *easter(int size, void (*var1) (void), void (*var2) (void), void (*var3) (void));
+
+#endif //SCRABBLE_FUNCTIONS_H
