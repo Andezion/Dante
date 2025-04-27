6a309e4d3ee25e65a283cee3221b4ff692f347c7
Andezion <115638748+Andezion@users.noreply.github.com>
2025-04-27 23:53:55 +0200

    Create functions.h

---
+++ Dante2/Module2.08/Zadanie1.05/functions.h
@@ -0,0 +1,10 @@
+#ifndef SOMESHIT_FUNCTIONS_H
+#define SOMESHIT_FUNCTIONS_H
+
+void free_texts(char **);
+char* lower_to_upper(const char *in);
+char* upper_to_lower(const char *in);
+char* space_to_dash(const char *in);
+char* reverse_letter(const char *in);
+
+#endif //SOMESHIT_FUNCTIONS_H
