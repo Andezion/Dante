38f7d788a8d9bad5c6ecc0ce1fc4d9b1cf0f3284
Andezion <115638748+Andezion@users.noreply.github.com>
2025-05-04 16:30:08 +0200

    Create functions.h

---
+++ Dante2/Module2.09/Zadanie1.12/functions.h
@@ -0,0 +1,8 @@
+#ifndef CROSSWORD_FUNCTIONS_H
+#define CROSSWORD_FUNCTIONS_H
+
+#define SQR(n) ((n)*(n))
+#define MIN(a, b) (a < b ? a : b)
+#define MAX(a, b) (a > b ? a : b)
+
+#endif //CROSSWORD_FUNCTIONS_H
