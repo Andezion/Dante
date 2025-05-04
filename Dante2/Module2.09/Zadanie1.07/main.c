3691695461c8a072b97cb23825d894c9f4a4689f
Andezion <115638748+Andezion@users.noreply.github.com>
2025-05-04 16:27:14 +0200

    Create main.c

---
+++ Dante2/Module2.09/Zadanie1.07/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+#define DISPLAY_BITS(value) for (int i = 63; i >= 0; --i) { printf("%lu", (num >> i) & 1); }
+
+void inverse_bits(void* ptr, size_t offset, size_t count);
+void show(const void* ptr, size_t count);
+
+int main()
+{
+
+    return 0;
+}
+
+void inverse_bits(void* ptr, size_t offset, size_t count)
+{
+    if(ptr == NULL || count < 1)
+    {
+        char *pointer = (char *) ptr;
+        for(size_t i = offset; i < count + offset; i++)
+        {
+            *(pointer + i * sizeof(char)) = ~(*(pointer + i * sizeof(char)));
+        }
+    }
+}
+
+void show(const void *ptr, size_t count)
+{
+    if(ptr == NULL)
+    {
+        return;
+    }
+
+    unsigned char letter;
+    char *pointer = (char *) ptr;
+
+    for(size_t i = 0; i < count; i++)
+    {
+        letter = *(pointer + i * sizeof(char));
+        printf("%02x ", letter);
+    }
+}
