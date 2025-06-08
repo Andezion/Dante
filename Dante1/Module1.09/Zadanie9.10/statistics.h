cfa2cae1bf59e2bf5a86720aaced89707805a34c
Andezion <115638748+Andezion@users.noreply.github.com>
2025-06-08 13:11:43 +0200

    Create statistics.h

---
+++ Dante1/Module1.09/Zadanie9.10/statistics.h
@@ -0,0 +1,8 @@
+#ifndef INC_9_10_STATISTICS_H
+#define INC_9_10_STATISTICS_H
+
+float median(int tab[], int size);
+float average(const int tab[], int size);
+int mode(int tab[], int size);
+
+#endif //INC_9_10_STATISTICS_H
