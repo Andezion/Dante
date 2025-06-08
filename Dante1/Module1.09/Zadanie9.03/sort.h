c98698ffeaa2993fc0080fd8db082eb029a56b64
Andezion <115638748+Andezion@users.noreply.github.com>
2025-06-08 13:03:17 +0200

    Update sort.h

--- Dante1/Module1.09/Zadanie9.03/sort.h
+++ Dante1/Module1.09/Zadanie9.03/sort.h
@@ -1,1 +1,10 @@
+#ifndef INC_9_MODUL_SORT_H
+#define INC_9_MODUL_SORT_H
 
+enum direction1 {ASCENDING = 1, DESCENDING = 2};
+
+int selection_sort_asc(int tab[], int size);
+int selection_sort_desc(int tab[], int size);
+int selection_sort(int tab[], int size, enum direction1 dir);
+
+#endif //INC_9_MODUL_SORT_H
