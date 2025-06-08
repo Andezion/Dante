8827e438c515f2d63911c05b5805989db2b49b04
Andezion <115638748+Andezion@users.noreply.github.com>
2025-06-08 13:05:30 +0200

    Create vector_utils.h

---
+++ Dante1/Module1.09/Zadanie9.04/vector_utils.h
@@ -0,0 +1,7 @@
+#ifndef INC_9_MODUL_VECTOR_UTILS_H
+#define INC_9_MODUL_VECTOR_UTILS_H
+
+int read_vector(int *tab, int size, int stop_value);
+void display_vector(const int* tab, int size);
+
+#endif //INC_9_MODUL_VECTOR_UTILS_H
