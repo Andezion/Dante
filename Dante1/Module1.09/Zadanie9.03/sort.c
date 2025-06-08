bc5f250b99a37e36cd3a6860eded1cf20d49c9bd
Andezion <115638748+Andezion@users.noreply.github.com>
2025-06-08 13:02:57 +0200

    Update sort.c

--- Dante1/Module1.09/Zadanie9.03/sort.c
+++ Dante1/Module1.09/Zadanie9.03/sort.c
@@ -1,1 +1,97 @@
+#include <stdio.h>
+#include "vector_utils.h"
+#include "sort.h"
+
+int selection_sort_asc(int tab[], int size)
+{
+    if(tab == NULL || size <= 0)
+    {
+        return 1;
+    }
+
+    if(size == 1)
+    {
+        printf("%i",tab[0]);
+        return 0;
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        int index = i;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (tab[j] < tab[index])
+            {
+                index = j;
+            }
+        }
+
+        int temp = tab[index];
+        tab[index] = tab[i];
+        tab[i] = temp;
+
+        display_vector(tab, size);
+    }
+    return 0;
+}
+
+
+int selection_sort_desc(int tab[], int size)
+{
+    if(tab == NULL || size <= 0)
+    {
+        return 1;
+    }
+
+    if(size == 1)
+    {
+        printf("%i",tab[0]);
+        return 0;
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        int index = i;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (tab[j] > tab[index])
+            {
+                index = j;
+            }
+        }
+
+        int temp = tab[index];
+        tab[index] = tab[i];
+        tab[i] = temp;
+
+        display_vector(tab, size);
+    }
+    return 0;
+}
+
+
+int selection_sort(int tab[], int size, enum direction1 dir)
+{
+    if(tab == NULL || size <= 0)
+    {
+        return 1;
+    }
+
+    if(size > 50)
+    {
+        size = 50;
+    }
+
+    if(dir == ASCENDING)
+    {
+        selection_sort_asc(tab,size);
+        return 0;
+    }
+    if(dir == DESCENDING)
+    {
+        selection_sort_desc(tab,size);
+        return 0;
+    }
+    return 1;
+}
 
