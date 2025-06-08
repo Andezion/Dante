411fa33a05ccf62d88c6bf0db6b04003356cce71
Andezion <115638748+Andezion@users.noreply.github.com>
2025-06-08 13:02:43 +0200

    Update main.c

--- Dante1/Module1.09/Zadanie9.03/main.c
+++ Dante1/Module1.09/Zadanie9.03/main.c
@@ -1,1 +1,44 @@
+#include <stdio.h>
+#include "vector_utils.h"
+#include "sort.h"
 
+
+int main()
+{
+    int vector[150];
+
+    printf("Enter: ");
+
+    int size = read_vector(vector, 150, 0);
+    if(size == -2)
+    {
+        printf("Incorrect input\n");
+        return 1;
+    }
+    if(size < 1)
+    {
+        printf("Not enough data available\n");
+        return 3;
+    }
+
+    int type;
+    printf("\nEnter: ");
+    int check = scanf("%i",&type);
+    if(check != 1)
+    {
+        printf("Incorrect input\n");
+        return 1;
+    }
+
+    printf("\n");
+
+    int check1 = selection_sort(vector, size, type);
+    if(check1 == 1)
+    {
+        printf("Incorrect input data\n");
+        return 2;
+    }
+
+
+    return 0;
+}
