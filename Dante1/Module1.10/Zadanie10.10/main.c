240fd0a83ae771555e634865729c77cab38d23fb
Andezion <115638748+Andezion@users.noreply.github.com>
2025-06-10 23:44:30 +0200

    Create main.c

---
+++ Dante1/Module1.10/Zadanie10.10/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int tab[] = {67305985, 134678021, 202050057};
+
+    char *bytes = (char *)tab;
+    for (size_t i = 0; i < sizeof(tab); i++)
+    {
+        if (*(bytes + i) >= 4 && *(bytes + i) <= 7)
+        {
+            printf("%p ", (void *)(bytes + i));
+        }
+    }
+    return 0;
+}
