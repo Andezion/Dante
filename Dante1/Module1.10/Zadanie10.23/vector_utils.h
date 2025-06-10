00c6dfd75b859973ef5abf3cbaa46690326cd2fa
Andezion <115638748+Andezion@users.noreply.github.com>
2025-06-10 23:56:28 +0200

    Create vector_utils.h

---
+++ Dante1/Module1.10/Zadanie10.23/vector_utils.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_C_VECTOR_UTILS_H
+#define MAIN_C_VECTOR_UTILS_H
+
+int read_vector_ll(long long *tab, int size, int stop_value);
+void display_vector_ll(const long long* tab, int size);
+
+int product_left(const long long *input, int input_size, long long *output, int output_size);
+int product_right(const long long *input, int input_size, long long *output, int output_size);
+
+#endif //MAIN_C_VECTOR_UTILS_H
