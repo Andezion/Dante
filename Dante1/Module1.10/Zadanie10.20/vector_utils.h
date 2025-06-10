dbb2e0170a5865a4e90b3fb2ba0eddc92b9be141
Andezion <115638748+Andezion@users.noreply.github.com>
2025-06-10 23:54:26 +0200

    Create vector_utils.h

---
+++ Dante1/Module1.10/Zadanie10.20/vector_utils.h
@@ -0,0 +1,9 @@
+#ifndef NAPISY_VECTOR_UTILS_H
+#define NAPISY_VECTOR_UTILS_H
+
+int read_vector(int *tab, int size, int stop_value);
+void display_vector(const int* tab, int size);
+
+int find(const int* tab, int size, int to_find);
+
+#endif //NAPISY_VECTOR_UTILS_H
