53dabf8b91ac0c97fc5dfd803c804ae3b78b0e10
Andezion <115638748+Andezion@users.noreply.github.com>
2025-06-23 12:15:49 +0200

    Create main.rs

---
+++ Dante1/Module1.01/Zadanie1.07/main.rs
@@ -0,0 +1,11 @@
+fn main()
+{
+    let f_pi: f32 = 3.141592653589793f32;
+    let d_pi: f64 = 3.141592653589793f64;
+    
+    println!("
+    pi is {}
+    d_pi is {}", f_pi, d_pi);
+    
+    return;
+}
