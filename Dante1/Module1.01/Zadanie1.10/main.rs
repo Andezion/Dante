8e46418b928aec740068c98415b64cde3ab3e59d
Andezion <115638748+Andezion@users.noreply.github.com>
2025-06-23 12:26:19 +0200

    Create main.rs

---
+++ Dante1/Module1.01/Zadanie1.10/main.rs
@@ -0,0 +1,16 @@
+fn main()
+{
+    let number_one: f32 = 5.5;
+    let number_two: f32 = 4.5;
+    
+    println!("The value of number_one is: {}", number_one);
+    println!("The value of number_two is: {}", number_two);
+    
+    println!("The sum is: {}", number_one + number_two);
+    println!("The difference is: {}", number_two - number_one);
+    println!("The multiplication is: {}", number_two * number_one);
+    
+    println!("The division is: {:.2}", number_one / number_two);
+    
+    return;
+}
