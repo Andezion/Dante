6e48b0ed373ec630d7511a07d05210241ea98b5c
Andezion <115638748+Andezion@users.noreply.github.com>
2025-06-23 12:11:38 +0200

    Create main.rs

---
+++ Dante1/Module1.01/Zadanie1.05/main.rs
@@ -0,0 +1,14 @@
+fn main()
+{
+    let num_integer: i32 = 5;
+    let num_double: f64 = 4.5;
+    let num_float: f32 = 3.5;
+    
+    println!(
+    "
+    Integer is {num_integer}
+    Double is {num_double}
+    Float is {num_float}");
+    
+    return;
+}
