5905e3d58f2ae03df990f9a784bc2abf328634a3
Andezion <115638748+Andezion@users.noreply.github.com>
2025-06-24 17:15:35 +0200

    Create main.rs

---
+++ Dante1/Module1.03/Zadanie3.02/main.rs
@@ -0,0 +1,14 @@
+fn main()
+{
+    let mut i: u32 = 0;
+    loop
+    {
+        if i == 101
+        {
+            break;
+        }
+        println!("{i}");   
+        i = i + 1;
+    }
+    return;
+}
