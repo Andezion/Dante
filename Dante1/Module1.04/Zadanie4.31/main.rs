073c27ea30cacd2d1c7ece56882bb6f959c5b347
Andezion <115638748+Andezion@users.noreply.github.com>
2025-08-26 22:51:51 +0200

    Create main.rs

---
+++ Dante1/Module1.04/Zadanie4.31/main.rs
@@ -0,0 +1,51 @@
+fn main() {
+    let mut input: String = String::new();
+
+    let mut size_one: usize = 0;
+    let mut array_one:[i32; 100] = [0; 100];
+    println!("Enter: ");
+
+    for _i in 0..100 {
+        input.clear();
+        std::io::stdin().read_line(&mut input).expect("Incorrect input");
+        array_one[size_one] = input.trim().parse().expect("Incorrect parsing");
+        if array_one[size_one] == 0 {
+            break;
+        }
+        size_one += 1;
+    }
+
+    let mut size_two: usize = 0;
+    let mut array_two:[i32; 100] = [0; 100];
+    println!("Enter: ");
+
+    for _i in 0..100 {
+        input.clear();
+        std::io::stdin().read_line(&mut input).expect("Incorrect input");
+        array_two[size_two] = input.trim().parse().expect("Incorrect parsing");
+        if array_two[size_two] == 0 {
+            break;
+        }
+        size_two += 1;
+    }
+
+    for i in 0..size_one - 1 {
+        let mut counter: u8 = 0;
+        for j in 0..size_two {
+            if array_one[i] > array_one[i + 1] {
+                if array_one[i + 1] < array_two[j] && array_one[i] > array_two[j] {
+                    counter += 1;
+                }
+            }
+            else if array_one[i] < array_one[i + 1] {
+                if array_one[i] < array_two[j] && array_one[i + 1] > array_two[j] {
+                    counter += 1;
+                }
+            }
+            else {
+                continue;
+            }
+        }
+        println!("{} ", counter);
+    }
+}
